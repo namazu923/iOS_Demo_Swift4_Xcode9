194de2add840e003aa5a5edb531b977ec966ef1e
namazu923 <namazu923@gmail.com>
2017-10-20 22:07:42 +0800

    init Project

---
+++ iOS_Demo/iOS_DemoTests/iOS_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_DemoTests.swift
+//  iOS_DemoTests
+//
+//  Created by Namazu on 2017/10/20.
+//  Copyright © 2017年 namazu923. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Demo
+
+class iOS_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
